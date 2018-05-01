435f0eb7704dcc663ff35789728df4d80e2a1045
fjrodriguez <fjrodriguez@mega-media.es>
2018-05-01 22:19:12 +0200

    Add mutation

---
+++ src/data/pokemon/index.ts
@@ -0,0 +1,23 @@
+import { Schema } from 'mongoose';
+import * as mongoose from 'mongoose';
+
+const PokemonSchema = new Schema({
+  id: Schema.Types.ObjectId,
+  number: Number,
+  name: String,
+  height: Number,
+  weight: Number,
+})
+
+const PokemonModel = mongoose.model('Pokemon', PokemonSchema);
+
+export const getPokemon = ({limit, skip}) => PokemonModel
+  .find({}, (err, pokemon) => pokemon)
+  .limit(limit)
+  .skip(skip)
+  .sort({number: 'asc'})
+;
+
+export const setPokemon = (args) => PokemonModel
+  .create(args)
+;
--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,48 +1,21 @@
 import { buildSchema } from 'graphql';
 import {
   GraphQLObjectType,
-  GraphQLNonNull,
-  GraphQLSchema,
-  GraphQLString,
-  GraphQLList,
-  GraphQLInt,
-  GraphQLBoolean
+  GraphQLSchema
 } from 'graphql/type';
-import PokemonSchema from '../models/pokemon';
-
-const PokemonType = new GraphQLObjectType({
-  name: 'pokemon',
-  fields: () => ({
-    id: {
-      type: GraphQLInt,
-    },
-    number: {
-      type: GraphQLInt,
-    },
-    name: {
-      type: GraphQLString,
-    },
-    height: {
-      type: GraphQLInt,
-    },
-    weight: {
-      type: GraphQLInt,
-    }
-  })
-});
+import { PokemonQueryField, PokemonMutationField } from './pokemon';
 
 const schema = new GraphQLSchema({
   query: new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
-      pokemon: {
-        type: new GraphQLList(PokemonType),
-        resolve: () => PokemonSchema
-          .find({}, (err, pokemon) => pokemon)
-          .limit(3)
-          .skip(0)
-          .sort({number: 'desc'})
-      }
+      pokemon: PokemonQueryField,
+    }
+  }),
+  mutation: new GraphQLObjectType({
+    name: 'RootMutationType',
+    fields: {
+      createPokemon: PokemonMutationField,
     }
   })
 })
