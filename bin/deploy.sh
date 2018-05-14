6afb09789e03cf6db135f13936ae898ac7183b84
Javier R. Blazquez <jrblazquez@gmail.com>
2018-05-15 01:11:16 +0200

    Deploy.sh modified

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
 if [ "${TRAVIS_PULL_REQUEST}" == "false" ] && [ "${TRAVIS_BRANCH}" == "master" ]; then
-
-  echo "Test after success"
+  git config user.name $GH_USER_NAME
+  git config user.email $GH_USER_EMAIL
+  git add --all .
+  git commit -m "Deploy from Travis - build {$TRAVIS_BUILD_NUMBER}"
+  openssl aes-256-cbc -K $encrypted_45bdefbbb50d_key -iv $encrypted_45bdefbbb50d_iv -in github_deploy_key.enc -out github_deploy_key -d
+  eval "$(ssh-agent -s)"
+  chmod 600 github_deploy_key.pem
+  ssh-add github_deploy_key.pem
+  echo "Sends build"
+  git push -f deploy master
 fi
