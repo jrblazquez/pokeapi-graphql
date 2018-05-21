d8c50d0579883632fd3f8a35690141ff29ddce6b
Francisco Javier Rodríguez Blázquez <fjrodriguez@megamedia.es>
2018-05-21 15:20:32 +0200

    change key

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -9,10 +9,12 @@ git config user.name $GH_USER_NAME
 git config user.email $GH_USER_EMAIL
 git add --all .
 git commit -m "Deploy from Travis - build {$TRAVIS_BUILD_NUMBER}"
-openssl aes-256-cbc -K $encrypted_45bdefbbb50d_key -iv $encrypted_45bdefbbb50d_iv -in .travis/github_deploy_key.enc -out github_deploy_key.pem -d
-eval "$(ssh-agent -s)"
-chmod 600 github_deploy_key.pem
-ssh-add github_deploy_key.pem
+openssl aes-256-cbc -K $encrypted_603b04cbb5e7_key -iv $encrypted_603b04cbb5e7_iv -in id_rsa_deploy.enc -out id_rsa_deploy -d
+# openssl aes-256-cbc -K $encrypted_45bdefbbb50d_key -iv $encrypted_45bdefbbb50d_iv -in .travis/github_deploy_key.enc -out github_deploy_key.pem -d
+# eval "$(ssh-agent -s)"
+# chmod 600 github_deploy_key.pem
+# ssh-add github_deploy_key.pem
+mv id_rsa_deploy-key ~/.ssh/id_rsa
 echo "Sends build"
 git show-ref
 git push -f integration master
