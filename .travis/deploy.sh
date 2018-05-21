761d4c49d2a05d1ab7731fbccd232100e79ed5ad
Francisco Javier Rodríguez Blázquez <fjrodriguez@megamedia.es>
2018-05-21 12:47:30 +0200

    Change path github_deploy_key

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -9,7 +9,7 @@ git config user.name $GH_USER_NAME
 git config user.email $GH_USER_EMAIL
 git add --all .
 git commit -m "Deploy from Travis - build {$TRAVIS_BUILD_NUMBER}"
-openssl aes-256-cbc -K $encrypted_45bdefbbb50d_key -iv $encrypted_45bdefbbb50d_iv -in github_deploy_key.enc -out github_deploy_key -d
+openssl aes-256-cbc -K $encrypted_45bdefbbb50d_key -iv $encrypted_45bdefbbb50d_iv -in .travis/github_deploy_key.enc -out github_deploy_key -d
 eval "$(ssh-agent -s)"
 chmod 600 github_deploy_key.pem
 ssh-add github_deploy_key.pem
