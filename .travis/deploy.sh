63efdd235620cde2aa1d2142ae43f2ed78a4e473
Francisco Javier Rodríguez Blázquez <fjrodriguez@megamedia.es>
2018-05-21 16:22:19 +0200

    final

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -7,10 +7,10 @@ git init
 git remote add integration $GH_URI
 git config user.name $GH_USER_NAME
 git config user.email $GH_USER_EMAIL
-git branch dev
-git checkout dev
 git add --all .
 git commit -m "Deploy from Travis - build {$TRAVIS_BUILD_NUMBER}"
+git branch dev
+git checkout dev
 openssl aes-256-cbc -K $encrypted_603b04cbb5e7_key -iv $encrypted_603b04cbb5e7_iv -in id_rsa_deploy.enc -out id_rsa_deploy -d
 # openssl aes-256-cbc -K $encrypted_45bdefbbb50d_key -iv $encrypted_45bdefbbb50d_iv -in .travis/github_deploy_key.enc -out github_deploy_key.pem -d
 eval "$(ssh-agent -s)"
